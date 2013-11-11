4c560b4857831708e6ed6c2d36f08cc09b1efc92
JohnnyJokera <nikitenkojane@rambler.ru>
2013-11-11 12:32:49 +0200

    new version src

--- project/src/test/java/com/epam/tests/TestBase.java
+++ project/src/test/java/com/epam/tests/TestBase.java
@@ -19,7 +19,11 @@ public class TestBase extends Configuration {
 	public void setUp() {
 		Reporter.log("[LOG]" + " " + "Starting test "
 				+ this.getClass().getSimpleName() + "<br>");
-		driver = WebDriverFactory.getDriver(DesiredCapabilities.firefox());
+		
+		DesiredCapabilities caps = new DesiredCapabilities();
+		caps.setBrowserName(System.getProperty("webdriver.browser", "firefox"));
+		
+		driver = WebDriverFactory.getDriver(caps);
 		Reporter.log("[LOG]" + " " + "Run base url<br>");
 		baseUrl = "http://pn.com.ua/";
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
