7db80bb9ee6bb39aa315d0552538685a4d7c77b8
JohnnyJokera <nikitenkojane@rambler.ru>
2013-11-13 17:39:35 +0200

    bggzdg

--- project/src/main/java/com/epam/config/Configuration.java
+++ project/src/main/java/com/epam/config/Configuration.java
@@ -5,8 +5,12 @@ import org.openqa.selenium.remote.DesiredCapabilities;
 
 public class Configuration {
 
-
+	private static DesiredCapabilities driver = new DesiredCapabilities();
 	protected String baseUrl = "http://pn.com.ua/";
 
+	public static Capabilities getDriver(){
+		driver.setBrowserName(System.getProperty("webdriver.browser","firefox"));
+		return driver;
+	}
 
 }
--- project/src/test/java/com/epam/tests/TestBase.java
+++ project/src/test/java/com/epam/tests/TestBase.java
@@ -1,13 +1,16 @@
 package com.epam.tests;
 
 import static com.epam.helper.TestBaseHelper.log;
+
 import java.util.concurrent.TimeUnit;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.testng.annotations.BeforeTest;
 
 import com.epam.config.Configuration;
 import com.epam.factories.WebDriverFactory;
+import com.epam.helper.ScreenShotOnFailure;
 
 public class TestBase extends Configuration {
 
@@ -22,6 +25,7 @@ public class TestBase extends Configuration {
 		cap.setBrowserName(System.getProperty("webdriver.browser","firefox"));
 				
 		driver = WebDriverFactory.getDriver(cap);
+		ScreenShotOnFailure.setDriver(driver);
 		log("[LOG]" + " " + "Run base url<br>");
 		baseUrl = "http://pn.com.ua/";
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
